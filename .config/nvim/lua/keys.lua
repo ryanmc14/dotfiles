f548138600f9651fa4a83db0d82185e76ce0d3ed
Ryan Mcleavy <ryanmcleavy14@gmail.com>
2024-04-12 23:58:26 +0100

    fix  keybinds for harpoon2

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -16,9 +16,21 @@ map('v', 'J', ":m '>+1<CR>gv=gv", {})
 -- undo / redo
 map('n','U', "<C-R>", {})
 -- harpoon
-map("n", "<leader>a", ":lua require('harpoon.mark').add_file()<CR>", {})
 map("n", "<leader>s", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", {})
 map("n", "<leader>1", ":lua require('harpoon.ui').nav_file(1)<CR>", {})
 map("n", "<leader>2", ":lua require('harpoon.ui').nav_file(2)<CR>", {})
 map("n", "<leader>3", ":lua require('harpoon.ui').nav_file(3)<CR>", {})
 map("n", "<leader>4", ":lua require('harpoon.ui').nav_file(4)<CR>", {})
+
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>s", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
