fa2e957efdee6768e13072c92afad3127a85671a
Ryan Mcleavy <ryanmcleavy14@gmail.com>
2024-11-30 19:39:17 +0000

    more changes

--- .config/nvim/lua/plugins/pomo.lua
+++ .config/nvim/lua/plugins/pomo.lua
@@ -2,12 +2,23 @@ return {
   "epwalsh/pomo.nvim",
   version = "*",  -- Recommended, use latest release instead of latest commit
   lazy = true,
-  cmd = { "TimerStart", "TimerRepeat" },
+  cmd = { "TimerStart", "TimerRepeat", "TimerSession" },
   dependencies = {
     -- Optional, but highly recommended if you want to use the "Default" timer
     "rcarriga/nvim-notify",
   },
   opts = {
     -- See below for full list of options 👇
+  sessions = {
+    pomodoro = {
+      { name = "Work", duration = "25m" },
+      { name = "Short Break", duration = "5m" },
+      { name = "Work", duration = "25m" },
+      { name = "Short Break", duration = "5m" },
+      { name = "Work", duration = "25m" },
+      { name = "Long Break", duration = "15m" },
+    },
+},
   },
+
 }
--- .config/nvim/lua/tele_cfg.lua
+++ .config/nvim/lua/tele_cfg.lua
@@ -12,6 +12,7 @@ vim.keymap.set('n', '<leader>gc', builtin.git_bcommits, {desc = "git_bcommits"})
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {desc = "git_branches"})
 vim.keymap.set('n', '<leader>gs', builtin.git_status, {desc = "git_status"})
 vim.keymap.set('n', '<leader>fh', ':Telescope find_files hidden=true<cr>', {desc = "find hidden files"})
+vim.keymap.set('n', '<leader>cs', builtin.colorscheme, {desc = "colourscheme"})
 
 -- You don't need to set any of these options.
 -- IMPORTANT!: this is only a showcase of how you can set default options!
@@ -32,3 +33,12 @@ require("telescope").setup {
     },
   },
 }
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = ""
+vim.opt.foldcolumn = "0"
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 4
+vim.opt.foldenable = false
