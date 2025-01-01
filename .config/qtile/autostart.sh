570170f984cc32722430aaff06a7847011003e08
Ryan Mcleavy <ryanmcleavy14@gmail.com>
2025-01-01 19:47:03 +0000

    picom fixes

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 xcape -e Super_L='Alt_L|F1' &
-picom -b &
+picom -b  --config /home/rmcle/.config/picom.conf &
 redshift -l 51.572309:0.397208 &
+xrandr --output DP-0 --mode 2560x1440 --rate 144.00 &
