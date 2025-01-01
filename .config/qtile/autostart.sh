b84a6294246cc409df82937e0fd9bc9a36ded651
Ryan Mcleavy <ryanmcleavy14@gmail.com>
2025-01-01 20:02:33 +0000

    update redshift values

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 xcape -e Super_L='Alt_L|F1' &
 picom -b  --config /home/rmcle/.config/picom.conf &
-redshift -l 51.572309:0.397208 &
+redshift -l 51.572309:0.397208 -t 6500:3800 &
 xrandr --output DP-0 --mode 2560x1440 --rate 144.00 &
